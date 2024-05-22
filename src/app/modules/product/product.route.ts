5389702094ed1ba6f6f11a4eeb65e44a1a19ddaa
Amhjadkhan <amjadpogramer@gmail.com>
2024-05-22 20:17:47 +0400

    this is my product route conmmit

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -3,17 +3,17 @@ import { ProductControllers } from './product.controller';
 
 const router = express.Router();
 
-router.post('/api/products', ProductControllers.createProduct);
+router.post('/products', ProductControllers.createProduct);
 
-router.get('/api/products', ProductControllers.getAllProducts);
+router.get('/products', ProductControllers.getAllProducts);
 
-router.get('/api/products/:productId', ProductControllers.getProductById);
+router.get('/products/:productId', ProductControllers.getProductById);
 
-router.put('/api/products/:productId', ProductControllers.updateProductById);
+router.put('/products/:productId', ProductControllers.updateProductById);
 
-router.delete('/api/products/:productId', ProductControllers.deleteProductById);
+router.delete('/products/:productId', ProductControllers.deleteProductById);
 
-router.get('/api/products/search', ProductControllers.searchProducts);
+router.get('/products/search', ProductControllers.searchProducts);
 
 
 export const ProductRoutes = router;
