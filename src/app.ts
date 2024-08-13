a0dda4d01283f748f5f1b2762f8cb7f38462f6fc
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-13 17:11:14 +0400

    this is some isue in server solve commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response, Application } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import { ProductRoutes } from './app/modules/product/product.route';
+import { orderRoutes } from './app/modules/order/order.route';
 // const port = 3000;
 
 // parsers
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/products', ProductRoutes)
+
+app.use('/api/orders', orderRoutes)
+
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('my eCommare port is running1y ttt'); 
--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -3,8 +3,8 @@ import { createOrder, getAllOrders, getOrdersByEmail } from './order.controller'
 
 const router = Router();
 
-router.post('/orders', createOrder);
-router.get('/orders', getAllOrders);
-router.get('/orders/email', getOrdersByEmail);
+router.post('/', createOrder);
+router.get('/', getAllOrders);
+router.get('/email', getOrdersByEmail);
 
-export default router;
+export const orderRoutes = router;
